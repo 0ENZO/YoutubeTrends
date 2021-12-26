f795bbeb31d4988c522c9e345c21926096148864
Thomas Tresgots <ttresgots@gmail.com>
2021-12-26 01:52:16 +0100

    1st POC to link csv and JSON; clean read both types and create join request

--- src/main/scala/App/Application.scala
+++ src/main/scala/App/Application.scala
@@ -1,8 +1,11 @@
 package App
 
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 import readers.{CsvReader, JsonReader}
+
+import java.io.FileNotFoundException
+import scala.sys.exit
 
 
 object Application {
@@ -18,8 +21,37 @@ object Application {
       .master("local[*]")
       .getOrCreate()
 
-    val df = new CsvReader(spark).read("data/CAvideos.csv")
-    val df2 = new JsonReader(spark).read("data/CA_category_id.json")
+    val (dfCategories, dfVideos) = readFiles(spark)
 
+    println("dfCategories")
+    dfCategories.show(1, truncate = false)
+
+    println("dfVideos")
+    dfVideos.show(1, truncate = false)
+
+    val requested_category = dfCategories.first().getAs[String]("id")
+    println(s"requested categ : $requested_category")
+    val videosOfCategory1 = dfVideos.filter(s"category_id == ${requested_category}")
+
+    videosOfCategory1.show(10, truncate = false)
+    println(videosOfCategory1.count())
+    videosOfCategory1.foreach(x => {
+      println(x.getAs[String]("title"))
+    })
+  }
+
+  def readFiles(spark: SparkSession): (DataFrame, DataFrame) = {
+    try {
+      val df = new JsonReader(spark).read("data/CA_category_id.json")
+
+      val df2 = new CsvReader(spark).read("data/CAvideos.csv")
+      (df, df2)
+    }
+    catch {
+      case e: FileNotFoundException => {
+        println("The requested file surely does not exist")
+        exit(1)
+      }
+    }
   }
 }
